fe6624299eae100da6f2ef40cf50a35365cb7a79
Michael Manlulu <michael.manlulu@myresman.com>
2022-10-11 07:35:18 +0800

    62. navigating with the "link" component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <ul>
+        <li>
+          <Link href='/portfolio'>Portfolio</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
