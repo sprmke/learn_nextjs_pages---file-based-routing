18d620ae6551992a1c4ec4e31d11381c3318d8ba
Michael Manlulu <michael.manlulu@myresman.com>
2022-10-11 08:42:19 +0800

    63. navigating to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,5 +1,23 @@
+import Link from 'next/link';
+
 const ClientsPage = () => {
-  return <div>ClientsPage</div>;
+  const clients = [
+    { id: 'mike', name: 'Mike' },
+    { id: 'aring', name: 'Aring' },
+  ];
+
+  return (
+    <div>
+      <h1>ClientsPage</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ClientsPage;
