a2ed104a70a10754063863e7dec57739b2de0a9c
Michael Manlulu <michael.manlulu@myresman.com>
2022-10-11 09:00:20 +0800

    65. navigating programmatically

--- pages/clients/[id]/[clientProjectId].js
+++ pages/clients/[id]/[clientProjectId].js
@@ -2,8 +2,12 @@ import { useRouter } from 'next/router';
 
 const ClientProjectPage = () => {
   const router = useRouter();
-  const { clientProjectId } = router.query;
-  return <div>ClientProjectPage - {clientProjectId}</div>;
+  const { id, clientProjectId } = router.query;
+  return (
+    <div>
+      ClientProjectPage - {id} - {clientProjectId}
+    </div>
+  );
 };
 
 export default ClientProjectPage;
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,5 +1,24 @@
+import { useRouter } from 'next/router';
+
 const ClientProjectsPage = () => {
-  return <div>ClientProjectsPage</div>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  const loadProjectHandler = () => {
+    // router.replace('/clients/mike/project-a');
+    // router.push('/clients/mike/project-a');
+    router.push({
+      pathname: '/clients/[id]/[clientProjectId]',
+      query: { id: id, clientProjectId: 'project-a' },
+    });
+  };
+
+  return (
+    <div>
+      <h1>The Projects of a given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 };
 
 export default ClientProjectsPage;
